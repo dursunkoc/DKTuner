52b05e1d6cd9ce59dad94bd04009007baddd2830
dursunkoc <dursunKoc@gmail.com>
2014-06-13 00:19:37 +0300

    Initial Commit

---
+++ DKTuner/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DKTuner
+//
+//  Created by dursun koc on 6/13/14.
+//  Copyright (c) 2014 dursunkoc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DKAppDelegate class]));
+    }
+}
